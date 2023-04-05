a42bd1055717b9bd26208a7dd3031ae8adecae8b
Nicky Kriplani <aniketnickykriplani@gmail.com>
2023-04-05 14:26:03 -0400

    changed tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders graph svg', () => {
   render(<App />);
-  const svg_element = screen.getByClassName('graph-display');
+  const svg_element = screen.getByRole('img');
   expect(svg_element).toBeInTheDocument();
 });
