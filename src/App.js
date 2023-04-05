72c7f35cdad8a28b899d22231368c6c4d4ac3e37
Nicky Kriplani <aniketnickykriplani@gmail.com>
2023-04-05 13:17:53 -0400

    making it compile, part 2

--- src/App.js
+++ src/App.js
@@ -24,12 +24,13 @@ const centerX = innerWidth / 2;
 const centerY = innerHeight / 2;
 const radius = 7;
 
+/*
 const boundsCheck = (value, bound) => {
   if(value < radius) return radius + 10;
   if(value > bound - radius) return bound - 10;
   return value;
 }
-
+*/
 
 
 const App = () => {
