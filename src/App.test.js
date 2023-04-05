ba0be8e4f93782b656ed1991ab9d6d4be6758d89
Nicky Kriplani <aniketnickykriplani@gmail.com>
2023-04-05 14:29:25 -0400

    rough test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders graph svg', () => {
-  render(<App />);
-  const svg_element = screen.getByRole('img');
+  const {container} = render(<App />);
+  const svg_element = container.querySelector('graph-display');
   expect(svg_element).toBeInTheDocument();
 });
